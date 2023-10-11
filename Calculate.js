8e201c0d4d99eb4d0079aaaf81c3f89245728ff3
Taimoor Qayyum <taimoorqayyum6@gmail.com>
2023-10-11 22:00:51 +0500

    Update Calculate.js
    Find the percentage

--- Calculate.js
+++ Calculate.js
@@ -1,13 +1,13 @@
-const num1 = 10
-const num2 = 30
+const totalNum = 1100
+const obtNum = 860
 
-const sum = num1 + num2
+const sum = (obtNum / totalNum) * 100
 
 console.log(sum)
 
-const sum1 = num1 / num2
+// const sum1 = num1 / num2
 
-console.log(sum1)
+// console.log(sum1)
 
-const sub = num1 - num2
-console.log(sub)
+// const sub = num1 - num2
+// console.log(sub)
